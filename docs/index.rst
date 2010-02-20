bccbfc9d52dde74cc3bac09f81a1ca7274d7cc9d
Dmitry Shevchenko <dmishe@gmail.com>
2010-02-20 12:56:36 +0200

    latest south code

---
+++ docs/index.rst
@@ -0,0 +1,35 @@
+
+.. _index:
+
+South documentation
+===================
+
+South is a tool to provide consistent, easy-to-use and database-agnostic
+migrations for Django applications.
+
+This is the documentation for the current version (0.7); previous versions'
+documentation was written directly into our wiki, but is mostly a subset of
+what is written here.
+
+If you want to view the old docs (for example, if you need something that was
+only in 0.6) they're `still available <http://south.aeracode.org/wiki/OldDocumentation>`_.
+
+Contents
+--------
+
+.. toctree::
+   :maxdepth: 2
+   
+   whataremigrations
+   tutorial/index
+   databaseapi
+   convertinganapp
+   migrationstructure
+   dependencies
+   commands
+   unittests
+   ormfreezing
+   generics
+   extendingintrospection
+   fixtures
+   settings
---
+++ south/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class MigrationHistory(models.Model):
+    app_name = models.CharField(max_length=255)
+    migration = models.CharField(max_length=255)
+    applied = models.DateTimeField(blank=True)
+
+    @classmethod
+    def for_migration(cls, migration):
+        try:
+            return cls.objects.get(app_name=migration.app_label(),
+                                   migration=migration.name())
+        except cls.DoesNotExist:
+            return cls(app_name=migration.app_label(),
+                       migration=migration.name())
+
+    def get_migrations(self):
+        from south.migration.base import Migrations
+        return Migrations(self.app_name)
+
+    def get_migration(self):
+        return self.get_migrations().migration(self.migration)
+    
+    def __unicode__(self):
+        return "<%s: %s>" % (self.app_name, self.migration)
